0d25e6df06e4cb40b2089c010abc6e9d57a2255c
Jonathan Soifer <me@jonathansoifer.com>
2017-08-05 14:17:54 +0300

    Implement basic cart tracking array of Products
    Displays item added to the cart
    
    Add empty cart conditional text
    
    Buggy
    
    Fix arrow function in method definition, implement unique cart items

--- public/script.js
+++ public/script.js
@@ -1,16 +1,52 @@
+'use strict';
+
 new Vue({
+
   el: '#app',
+
   data: {
+
     total: 0,
+
     items: [
-      { title: 'Item 1' },
-      { title: 'Item 2' },
-      { title: 'Item 3' }
-    ]
+      { id: 1, title: 'Item 1' },
+      { id: 2, title: 'Item 2' },
+      { id: 3, title: 'Item 3' }
+    ],
+
+    cart: [],
+
   },
+
   methods: {
-    addItem: function() {
+
+    addItem(index) {
+
+      // Cart Total Cost
       this.total += 9.99;
+
+      const item = this.items[index];
+
+      // Is the item already on the list?
+      let found = false;
+
+      for (let i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          this.cart[i].qty++;
+          found = true;
+        }
+      }
+
+      if (!found) {
+        this.cart.push({
+          id: item.id, 
+          title: item.title,
+          qty: 1,
+        });
+      }
+
     }
+
   }
+
 });
