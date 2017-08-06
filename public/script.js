b4a03458f05df3f1ba1262cda9dfd5ac8521fcff
Jonathan Soifer <me@jonathansoifer.com>
2017-08-06 13:06:17 +0300

    Add item specific prices and display them on the cart

--- public/script.js
+++ public/script.js
@@ -9,9 +9,9 @@ new Vue({
     total: 0,
 
     items: [
-      { id: 1, title: 'Item 1' },
-      { id: 2, title: 'Item 2' },
-      { id: 3, title: 'Item 3' }
+      { id: 1, title: 'Item 1', price: 8.80 },
+      { id: 2, title: 'Item 2', price: 18.80  },
+      { id: 3, title: 'Item 3', price: 4.80 }
     ],
 
     cart: [],
@@ -30,6 +30,7 @@ new Vue({
       // Is the item already on the list?
       let found = false;
 
+      // Let's loop through the cart to check that
       for (let i = 0; i < this.cart.length; i++) {
         if (this.cart[i].id === item.id) {
           this.cart[i].qty++;
@@ -37,11 +38,13 @@ new Vue({
         }
       }
 
+      // If it was found, just update the quantity
       if (!found) {
         this.cart.push({
           id: item.id, 
           title: item.title,
           qty: 1,
+          price: item.price,
         });
       }
 
